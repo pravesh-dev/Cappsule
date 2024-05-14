400646a8029d52f01382dc6116d4a2747e71ede2
pravesh dav <humantorch1932@gmail.com>
2024-05-14 18:02:58 +0530

    showing dynamic data for strength for the default selection and added hide and show more options functionality for strengths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// https://backend.cappsule.co.in/api/v1/new_search?q=paracetamol&pharmacyIds=1,2,3
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { GoSearch } from "react-icons/go";
 import { FaArrowLeftLong } from "react-icons/fa6";
@@ -17,17 +16,82 @@ function App() {
   };
 
   const [textInput, setTextInput] = useState("");
-  const [height, setHeight] = useState('4.5rem')
+  const [height, setHeight] = useState("4.5rem");
   const isSearched = false;
-  const [isHidden, setIsHidden] = useState(true);
-  const handleShowMore = () =>{
-    setHeight('auto')
-    setIsHidden(false)
-  }
-  const handleHideMore = () =>{
-    setHeight('4.5rem')
-    setIsHidden(true)
-  }
+  const [isFormHidden, setIsFormHidden] = useState(true);
+  const [isStrengthHidden, setIsStrengthHidden] = useState(true);
+  const [selectedValues, setSelectedValues] = useState({});
+
+  useEffect(() => {
+    if (salts.length > 0) {
+      setInitialSelectedValues();
+    }
+  }, [salts]);
+
+  const handleShowMore = (type) => {
+    if (type === "form") {
+      setHeight("auto");
+      setIsFormHidden(false);
+    } else if (type === "strength") {
+      setHeight("auto");
+      setIsStrengthHidden(false);
+    }
+  };
+
+  const handleHideMore = (type) => {
+    if (type === "form") {
+      setHeight("4.5rem");
+      setIsFormHidden(true);
+    } else if (type === "strength") {
+      setHeight("4.5rem");
+      setIsStrengthHidden(true);
+    }
+  };
+
+  const setInitialSelectedValues = () => {
+    const initialValues = {};
+    salts.map((salt, index) => {
+      const firstForm = Object.keys(salt.salt_forms_json)[0];
+      const firstStrength = Object.keys(salt.salt_forms_json[firstForm])[0];
+      const firstPackaging = Object.keys(
+        salt.salt_forms_json[firstForm][firstStrength]
+      )[0];
+      initialValues[index] = {
+        form: firstForm,
+        strength: firstStrength,
+        packaging: firstPackaging,
+      };
+    });
+    setSelectedValues(initialValues);
+  };
+
+  const renderForms = (salt) => {
+    return Object.keys(salt.salt_forms_json).map((key, index) => (
+      <button
+        key={index}
+        className="px-3 py-1 text-sm border-2 border-black rounded-lg font-semibold"
+      >
+        {key}
+      </button>
+    ));
+  };
+
+  const renderStrengths = (salt, index) => {
+    if (!selectedValues[index]) return null;
+    const form = selectedValues[index].form;
+    return Object.keys(salt.salt_forms_json[form]).map(
+      (strength, strengthIndex) => {
+        return (
+          <button
+            key={strengthIndex}
+            className="px-2 py-1 text-sm border-2 border-black rounded-lg font-semibold"
+          >
+            {strength}
+          </button>
+        );
+      }
+    );
+  };
 
   return (
     <div className="w-full min-h-screen flex gap-10 flex-col items-center py-14">
@@ -39,7 +103,9 @@ function App() {
       </h1>
       <div className="shadow-[0_0_20px_#00000038] w-[70vw] h-14 rounded-[30px] flex justify-between items-center px-10">
         <div className="relative">
-          <GoSearch className={`text-2xl ${isSearched ? "hidden" : "block"}`} />
+          <GoSearch
+            className={`text-2xl ${isSearched ? "hidden" : "block"}`}
+          />
           <FaArrowLeftLong
             className={`text-2xl ${isSearched ? "block" : "hidden"}`}
           />
@@ -68,31 +134,59 @@ function App() {
                 className="w-full min-h-52 bg-gradient-to-r from-white to-blue-200/40 shadow-[0_0_13px_#00000028] rounded-2xl flex justify-between p-8"
               >
                 <div className="min-h-full w-[30%] flex flex-col gap-3 justify-between">
-                  <div className="flex gap-4 border relative">
+                  <div className="flex gap-4 relative">
                     <h3 className="w-20 text-black/90 mt-1">Form:</h3>
-                    <div className={`w-52 flex gap-1 flex-wrap ${salt.available_forms.length > 4 ? `h-[${height}] border overflow-hidden` : 'border-none'}`}>
-                      {salt.available_forms.map((form, formIndex) => (
-                        <button
-                          key={formIndex}
-                          className="px-3 py-1 text-sm border-2 border-black rounded-lg font-semibold"
-                        >
-                          {form}
-                        </button>
-                      ))}
+                    <div
+                      className={`w-52 flex gap-2 flex-wrap ${
+                        salt.available_forms.length > 4 &&
+                        `h-[${height}] overflow-hidden`
+                      }`}
+                    >
+                      {renderForms(salt)}
                     </div>
-                    <button className={`absolute left-full bottom-0 text-blue-900 font-bold ${salt.available_forms.length > 4 ? 'block' : 'hidden'}  ${isHidden ? 'block' : 'hidden'}`} onClick={handleShowMore}>more..</button>
-                    <button className={`absolute left-full bottom-0 text-blue-900 font-bold ${salt.available_forms.length > 4 ? 'block' : 'hidden'} ${isHidden ? 'hidden' : 'block'}`} onClick={handleHideMore}>hide..</button>
+                    <button
+                      className={`absolute left-full bottom-0 text-blue-900 font-bold ${
+                        salt.available_forms.length > 4 ? "block" : "hidden"
+                      }  ${isFormHidden ? "block" : "hidden"}`}
+                      onClick={() => handleShowMore("form")}
+                    >
+                      more..
+                    </button>
+                    <button
+                      className={`absolute left-full bottom-0 text-blue-900 font-bold ${
+                        salt.available_forms.length > 4 ? "block" : "hidden"
+                      } ${isFormHidden ? "hidden" : "block"}`}
+                      onClick={() => handleHideMore("form")}
+                    >
+                      hide..
+                    </button>
                   </div>
-                  <div className="flex gap-4">
+                  <div className="flex gap-4 relative">
                     <h3 className="w-20 text-black/90 mt-1">Strength:</h3>
-                    <div className=" w-52 flex gap-1 flex-wrap">
-                      <button className="px-2 py-1 text-sm border-2 border-black rounded-lg font-semibold">
-                        100 mg
-                      </button>
-                      <button className="px-3 py-1 text-sm border-2 border-black rounded-lg font-semibold">
-                        500 mg
-                      </button>
+                    <div
+                      className={`w-52 flex gap-1 flex-wrap ${
+                        Object.keys(salt.salt_forms_json[selectedValues[index]?.form] || {}).length > 4 &&
+                        `h-[${height}] overflow-hidden py-1`
+                      }`}
+                    >
+                      {renderStrengths(salt, index)}
                     </div>
+                    <button
+                      className={`absolute left-full bottom-0 text-blue-900 font-bold ${
+                        Object.keys(salt.salt_forms_json[selectedValues[index]?.form] || {}).length > 4 ? "block" : "hidden"
+                      }  ${isStrengthHidden ? "block" : "hidden"}`}
+                      onClick={() => handleShowMore("strength")}
+                    >
+                      more..
+                    </button>
+                    <button
+                      className={`absolute left-full bottom-0 text-blue-900 font-bold ${
+                        Object.keys(salt.salt_forms_json[selectedValues[index]?.form] || {}).length > 4 ? "block" : "hidden"
+                      } ${isStrengthHidden ? "hidden" : "block"}`}
+                      onClick={() => handleHideMore("strength")}
+                    >
+                      hide..
+                    </button>
                   </div>
                   <div className="flex gap-4">
                     <h3 className="w-20 text-black/90 mt-1">Packaging:</h3>
